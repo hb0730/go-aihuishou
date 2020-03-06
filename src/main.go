fc98d6805fa5b2ef485a96e7afe9344597d85b3c
黄兵 <1278032416@qq.com>
2020-03-06 14:37:02 +0800

    fix

--- src/main.go
+++ src/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"aihuishou"
-	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
+	"strconv"
 )
 
 type BaseModel struct {
@@ -13,14 +14,35 @@ type BaseModel struct {
 	Type  string
 }
 
+var file *os.File
+var htmlUrl []*aihuishou.HtmlUrl
+
 func main() {
-	url := new(aihuishou.HtmlUrl)
-	url.Url = "https://www.aihuishou.com/"
-	url.Type = "shouji"
-	url.Page = 135
-	list := aihuishou.ToGo(url)
-	for i := list.Front(); i != nil; i = i.Next() {
-		phone := i.Value.(*aihuishou.Phone)
-		fmt.Println(phone)
+	//创建excel文件
+	f, err := os.Create("D:/haha3.txt")
+	if err != nil {
+		panic(err)
 	}
+	file = f
+	defer f.Close()
+	htmlUrl = make([]*aihuishou.HtmlUrl, 3)
+	htmlUrl[0] = &aihuishou.HtmlUrl{Url: "https://www.aihuishou.com/", Type: "shouji", Page: 135}
+	htmlUrl[1] = &aihuishou.HtmlUrl{Url: "https://www.aihuishou.com/", Type: "pingban", Page: 35}
+	htmlUrl[2] = &aihuishou.HtmlUrl{Url: "https://www.aihuishou.com/", Type: "laptop", Page: 313}
+	write()
+
+}
+
+func write() {
+	file.WriteString("类型 \t 价格 \t\r\n")
+	for _, url := range htmlUrl {
+		list := aihuishou.ToGo(url)
+		file.WriteString(url.Type + "\t\r\n")
+		for i := list.Front(); i != nil; i = i.Next() {
+			phone := i.Value.(*aihuishou.Phone)
+			//fmt.Println(phone)
+			file.WriteString(phone.Name + "\t" + strconv.FormatFloat(phone.Price, 'g', 6, 64) + "\t" + "\r\n")
+		}
+	}
+
 }
